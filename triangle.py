1da8e5b1d274fd4145b4b97489ddc47e1a934430
Marco Mat <dlastgamer18@gmail.com>
2017-07-28 16:56:38 +0800

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,20 @@
+def run():
+    for i in range(5):
+        for x in range(i):
+            print('*', end=" ")
+
+        print(" ")
+
+    for a in range(5, 0, -1):
+        for j in range(a):
+            print('*', end=" ")
+
+        print(" ")
+        
+    
+        
+
+
+        
+
+        
